017e4df7c4337bc8bb1f161a82a9f607e39e85c6
Watt Wilkinson <wattwilkinson4@gmail.com>
2020-07-16 10:51:06 -0700

    changed the text of our app

---
+++ Rock paper scissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rock paper scissors
+//
+//  Created by Melissa Wilkinson on 7/16/20.
+//  Copyright © 2020 Watt Wilkinson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hi my name is watt")
+                .font(.largeTitle)
+                .foregroundColor(Color.red)
+           
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
